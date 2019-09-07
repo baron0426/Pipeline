6515bf6cb72e28a217e16373b8301253bc7016d4
baron0426 <baron0426@gmail.com>
2019-09-08 00:19:24 +0800

    add

--- Pipeline/exp04.runs/impl_3/CPU.tcl
+++ Pipeline/exp04.runs/impl_3/CPU.tcl
@@ -61,12 +61,144 @@ proc step_failed { step } {
 }
 
 
+start_step init_design
+set ACTIVE_STEP init_design
+set rc [catch {
+  create_msg_db init_design.pb
+  create_project -in_memory -part xc7a35tcpg236-3
+  set_property design_mode GateLvl [current_fileset]
+  set_param project.singleFileAddWarning.threshold 0
+  set_property webtalk.parent_dir D:/Baron/Pipeline/Pipeline/exp04.cache/wt [current_project]
+  set_property parent.project_path D:/Baron/Pipeline/Pipeline/exp04.xpr [current_project]
+  set_property ip_output_repo D:/Baron/Pipeline/Pipeline/exp04.cache/ip [current_project]
+  set_property ip_cache_permissions {read write} [current_project]
+  add_files -quiet D:/Baron/Pipeline/Pipeline/exp04.runs/synth_2/CPU.dcp
+  set_msg_config -source 4 -id {BD 41-1661} -limit 0
+  set_param project.isImplRun true
+  add_files D:/Baron/Pipeline/Pipeline/exp04.srcs/sources_1/bd/SortData/SortData.bd
+  set_param project.isImplRun false
+  read_xdc D:/Baron/Pipeline/Pipeline/exp04.srcs/constrs_1/new/constraint.xdc
+  set_param project.isImplRun true
+  link_design -top CPU -part xc7a35tcpg236-3
+  set_param project.isImplRun false
+  write_hwdef -force -file CPU.hwdef
+  close_msg_db -file init_design.pb
+} RESULT]
+if {$rc} {
+  step_failed init_design
+  return -code error $RESULT
+} else {
+  end_step init_design
+  unset ACTIVE_STEP 
+}
+
+start_step opt_design
+set ACTIVE_STEP opt_design
+set rc [catch {
+  create_msg_db opt_design.pb
+  opt_design 
+  write_checkpoint -force CPU_opt.dcp
+  create_report "impl_3_opt_report_drc_0" "report_drc -file CPU_drc_opted.rpt -pb CPU_drc_opted.pb -rpx CPU_drc_opted.rpx"
+  close_msg_db -file opt_design.pb
+} RESULT]
+if {$rc} {
+  step_failed opt_design
+  return -code error $RESULT
+} else {
+  end_step opt_design
+  unset ACTIVE_STEP 
+}
+
+start_step place_design
+set ACTIVE_STEP place_design
+set rc [catch {
+  create_msg_db place_design.pb
+  if { [llength [get_debug_cores -quiet] ] > 0 }  { 
+    implement_debug_core 
+  } 
+  place_design -directive ExtraTimingOpt
+  write_checkpoint -force CPU_placed.dcp
+  create_report "impl_3_place_report_io_0" "report_io -file CPU_io_placed.rpt"
+  create_report "impl_3_place_report_utilization_0" "report_utilization -file CPU_utilization_placed.rpt -pb CPU_utilization_placed.pb"
+  create_report "impl_3_place_report_control_sets_0" "report_control_sets -verbose -file CPU_control_sets_placed.rpt"
+  close_msg_db -file place_design.pb
+} RESULT]
+if {$rc} {
+  step_failed place_design
+  return -code error $RESULT
+} else {
+  end_step place_design
+  unset ACTIVE_STEP 
+}
+
+start_step phys_opt_design
+set ACTIVE_STEP phys_opt_design
+set rc [catch {
+  create_msg_db phys_opt_design.pb
+  phys_opt_design -directive Explore
+  write_checkpoint -force CPU_physopt.dcp
+  close_msg_db -file phys_opt_design.pb
+} RESULT]
+if {$rc} {
+  step_failed phys_opt_design
+  return -code error $RESULT
+} else {
+  end_step phys_opt_design
+  unset ACTIVE_STEP 
+}
+
+  set_msg_config -source 4 -id {Route 35-39} -severity "critical warning" -new_severity warning
+start_step route_design
+set ACTIVE_STEP route_design
+set rc [catch {
+  create_msg_db route_design.pb
+  route_design -directive NoTimingRelaxation
+  write_checkpoint -force CPU_routed.dcp
+  create_report "impl_3_route_report_drc_0" "report_drc -file CPU_drc_routed.rpt -pb CPU_drc_routed.pb -rpx CPU_drc_routed.rpx"
+  create_report "impl_3_route_report_methodology_0" "report_methodology -file CPU_methodology_drc_routed.rpt -pb CPU_methodology_drc_routed.pb -rpx CPU_methodology_drc_routed.rpx"
+  create_report "impl_3_route_report_power_0" "report_power -file CPU_power_routed.rpt -pb CPU_power_summary_routed.pb -rpx CPU_power_routed.rpx"
+  create_report "impl_3_route_report_route_status_0" "report_route_status -file CPU_route_status.rpt -pb CPU_route_status.pb"
+  create_report "impl_3_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file CPU_timing_summary_routed.rpt -pb CPU_timing_summary_routed.pb -rpx CPU_timing_summary_routed.rpx"
+  create_report "impl_3_route_report_incremental_reuse_0" "report_incremental_reuse -file CPU_incremental_reuse_routed.rpt"
+  create_report "impl_3_route_report_clock_utilization_0" "report_clock_utilization -file CPU_clock_utilization_routed.rpt"
+  create_report "impl_3_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file CPU_bus_skew_routed.rpt -pb CPU_bus_skew_routed.pb -rpx CPU_bus_skew_routed.rpx"
+  close_msg_db -file route_design.pb
+} RESULT]
+if {$rc} {
+  write_checkpoint -force CPU_routed_error.dcp
+  step_failed route_design
+  return -code error $RESULT
+} else {
+  end_step route_design
+  unset ACTIVE_STEP 
+}
+
+start_step post_route_phys_opt_design
+set ACTIVE_STEP post_route_phys_opt_design
+set rc [catch {
+  set tool_flow [get_property TOOL_FLOW [current_project]]
+  if {$tool_flow eq {SDx}} {send_msg_id {101-1} {status} {Starting optional post-route physical design optimization.} }
+  create_msg_db post_route_phys_opt_design.pb
+  phys_opt_design 
+  write_checkpoint -force CPU_postroute_physopt.dcp
+  create_report "impl_3_post_route_phys_opt_report_timing_summary_0" "report_timing_summary -max_paths 10 -warn_on_violation -file CPU_timing_summary_postroute_physopted.rpt -pb CPU_timing_summary_postroute_physopted.pb -rpx CPU_timing_summary_postroute_physopted.rpx"
+  create_report "impl_3_post_route_phys_opt_report_bus_skew_0" "report_bus_skew -warn_on_violation -file CPU_bus_skew_postroute_physopted.rpt -pb CPU_bus_skew_postroute_physopted.pb -rpx CPU_bus_skew_postroute_physopted.rpx"
+  close_msg_db -file post_route_phys_opt_design.pb
+  set tool_flow [get_property TOOL_FLOW [current_project]]
+  if {$tool_flow eq {SDx}} {send_msg_id {101-1} {status} {Finished optional post-route physical design optimization.} }
+} RESULT]
+if {$rc} {
+  step_failed post_route_phys_opt_design
+  return -code error $RESULT
+} else {
+  end_step post_route_phys_opt_design
+  unset ACTIVE_STEP 
+}
+
 start_step write_bitstream
 set ACTIVE_STEP write_bitstream
 set rc [catch {
   create_msg_db write_bitstream.pb
-  open_checkpoint CPU_postroute_physopt.dcp
-  set_property webtalk.parent_dir D:/youzq/Pipeline/Pipeline/exp04.cache/wt [current_project]
   catch { write_mem_info -force CPU.mmi }
   write_bitstream -force CPU.bit 
   catch { write_sysdef -hwdef CPU.hwdef -bitfile CPU.bit -meminfo CPU.mmi -file CPU.sysdef }
--- Pipeline/exp04.runs/synth_2/runme.sh
+++ Pipeline/exp04.runs/synth_2/runme.sh
@@ -24,7 +24,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='D:/youzq/Pipeline/Pipeline/exp04.runs/synth_2'
+HD_PWD='D:/Baron/Pipeline/Pipeline/exp04.runs/synth_2'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
